b07f2ffa566917e34fe4c981b823b00ddaf3d3a3
PiyushInnotech <121004823+PiyushInnotech@users.noreply.github.com>
2023-06-20 14:31:18 +0530

    Update firebaseConfig.js

--- src/env/firebaseConfig.js
+++ src/env/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDjRnxhGzjtqK31WB_OejBQZZHWQO2V9gQ",
-    authDomain: "vuex-login-d58ae.firebaseapp.com",
-    projectId: "vuex-login-d58ae",
-    storageBucket: "vuex-login-d58ae.appspot.com",
-    messagingSenderId: "147256602067",
-    appId: "1:147256602067:web:a3b7c6977f729d61e69326"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId:"",
+    appId: ""
 }
 
 initializeApp(firebaseConfig)
